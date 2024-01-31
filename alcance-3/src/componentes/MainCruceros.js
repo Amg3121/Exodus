4f41913eea5971684c5ee534e110e81872747740
Amg3121 <misaelg488@gmail.com>
2024-01-31 17:24:15 -0600

    Actualización alcance-3

--- alcance-3/src/componentes/MainCruceros.js
+++ alcance-3/src/componentes/MainCruceros.js
@@ -1,30 +1,27 @@
 import React from 'react'
-
 function MainCruceros() {
     return (
         <>
-        <section className="back-head back-head-red">
-			<div className="container">
-				<div className="row">
-					<div className="col-md-12 container-main-title">
-						<h1 className="text-center main-subtitle mb-1">Viajes Cruceros</h1>	
-					</div>
-				</div>
-			</div>
-			<div className="filter"></div>
-		</section>
-        <section className="section-top-ten">
-        <div className="container">
-            <div className="row">
-                <div className="col-md-12">
-                
-                <iframe className="iframe-mega" title="cruceros" src="https://www.megatravel.com.mx/tools/vi.php?Dest=13&txtColor=333333&aColor=333333&ahColor=f80b7d&thBG=000&thTxColor=fff&ff=9" width="100%" height="1200" border="0"align="left" allowtransparency="true" frameborder="0"> </iframe>
-                </div> 
-            </div> 
-        </div>   
-        </section>
+            <section className="back-head back-head-red">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12 container-main-title">
+                            <h1 className="text-center main-subtitle mb-1">Viajes Cruceros</h1>
+                        </div>
+                    </div>
+                </div>
+                <div className="filter"></div>
+            </section>
+            <section className="section-top-ten">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <iframe className="iframe-mega" title="cruceros" src="https://www.megatravel.com.mx/tools/vi.php?Dest=13&txtColor=333333&aColor=333333&ahColor=f80b7d&thBG=000&thTxColor=fff&ff=9" width="100%" height="1200" border="0" align="left" allowtransparency="true" frameborder="0"> </iframe>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </>
     )
 }
-
 export default MainCruceros
--- alcance-3/src/componentes/SectionAbout.js
+++ alcance-3/src/componentes/SectionAbout.js
@@ -3,30 +3,27 @@
  * Developed by David Marin
  */
 import React from 'react'
-
-function SectionAbout({user}) {
-    return (
-        <>
-        <section className="back-about back-about-red">
-			<div className="container-template">
-				<div className="row">
-					<div className="col-md-12">
-						<h3 className="text-center title-section mb-4">Quienes Somos...</h3>
-					</div>
-					<div className="col-md-12">
-						{user.map((dato, index) => {
-							return <p key={index} className="text-about" dangerouslySetInnerHTML={{__html: dato.description}}></p>;
-						})}
-					</div>
-					
-					<div className="col-md-12 text-center">
-						<a href="" className="btn-about btn-back-red mt-4">Empezar</a>
+function SectionAbout({ user }) {
+	return (
+		<>
+			<section className="back-about back-about-red">
+				<div className="container-template">
+					<div className="row">
+						<div className="col-md-12">
+							<h3 className="text-center title-section mb-4">Quienes Somos...</h3>
+						</div>
+						<div className="col-md-12">
+							{user.map((dato, index) => {
+								return <p key={index} className="text-about" dangerouslySetInnerHTML={{ __html: dato.description }}></p>;
+							})}
+						</div>
+						<div className="col-md-12 text-center">
+							<a href="" className="btn-about btn-back-red mt-4">Empezar</a>
+						</div>
 					</div>
 				</div>
-			</div>
-		</section>
-        </>
-    )
+			</section>
+		</>
+	)
 }
-
 export default SectionAbout
