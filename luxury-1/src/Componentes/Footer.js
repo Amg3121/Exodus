5d8a27031b1824ee4c5c03520133de045270dac4
Amg3121 <misaelg488@gmail.com>
2024-02-08 12:56:47 -0600

    actualizacion footer luxury-1

--- luxury-1/src/Componentes/Footer.js
+++ luxury-1/src/Componentes/Footer.js
@@ -19,6 +19,32 @@ function Footer({ user }) {
                                     </div>
                                 </div>
                             </div>
+                            {/**footer para atentas viajes 
+                            <div className="col-md-6">
+                                <div className="row">
+                                    <div className="col-md-6">
+                                        <h3 className="footer__title-h3">MATRIZ</h3>
+                                        <p className="footer__p">
+                                            Esquina Calle 30 Norte, C. 32 Nte, S/N <br/>
+                                            Col: Azteca - CP. 72310<br/>
+                                            Puebla, Puebla<br/>
+                                            Tel. Oficina: <a href='tel:2222366743'>2222366743</a><br/>
+                                            <a href='mailto:viajesatenas@yahoo.com.mx'>viajesatenas@yahoo.com.mx</a>
+                                        </p>
+                                    </div>
+                                    <div className="col-md-6">
+                                        <h3 className="footer__title-h3">SUCURSAL CHOLULA</h3>
+                                        <p className="footer__p">
+                                            {user.address}, {user.outdoor_number}<br/>
+                                            Col: {user.suburb} - CP. {user.postal_code}<br/>
+                                            {user.city_name}, {user.state_name}<br/>
+                                            Tel. Oficina:: <a href={'tel:' + user.phone}>{user.phone}</a><br/>
+                                            <a href={'mailto:' + user.agency_mail}>{user.agency_mail}</a>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            */}
                             <div className="col-md-6">
                                 <ul className="list-inline text-center list__ul-footer">
                                     {user.facebook != null ? <li className="list-inline-item">
